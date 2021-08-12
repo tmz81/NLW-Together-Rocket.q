90094a6a1c1ddc8c0effe8f00b1872c9e62c454d
tmz81 <tmz_81@hotmail.com>
2021-08-12 14:54:05 -0300

    upgrades back-end

--- src/route.js
+++ src/route.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const QuestionController = require('./controllers/QuestionController');
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"));
-route.get('/create-room', (req, res) => res.render("create-room"));
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}));
+route.get('/create-room', (req, res) => res.render("index", {page: 'create-room'}));
 route.get('/room', (req, res) => res.render("room"));
-//route.post('/room/:room/:question/:action', (req, res) => res.render())
+
+route.post('/room/:room/:question/:action', QuestionController.index)
+route.post('/room/create-room', RoomController.create)
 
 
 module.exports = route
