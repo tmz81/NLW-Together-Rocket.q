22895ce3a2cc51fcf3fcf354bae4b459b7d324c6
tmz81 <tmz_81@hotmail.com>
2021-08-16 19:45:01 -0300

    Modified files

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,22 +5,41 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
+    let isRoom = true
 
-    for (var i = 0; i < 6; i++){
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-      roomId += Math.floor(Math.random() * 10).toString()
+    while(isRoom){
+      /* Generation number room */
+      for (var i = 0; i < 6; i++){
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+        roomId += Math.floor(Math.random() * 10).toString()
+      }
+
+      /* Verification number room exist */
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+      
+      if(!isRoom){
+        // insert room in bank 
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
     }
-    
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
 
     await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+  async open(req, res){
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    
+    res.render("room", {roomId: roomId, questions : questions, questionsRead : questionsRead})
   }
 }
